b6b725560f0aee4dc045fe18de0d30e2997e95b3
Luc STEELANDT <luc.steelandt@gmail.com>
2019-03-20 17:35:36 +0100

    Create xiaomi.ht_sensor.js

---
+++ devices/xiaomi.ht_sensor.js
@@ -0,0 +1,26 @@
+let ELIGIBLE_MODEL_ID = ['lumi.sensor_ht'];
+
+module.exports = {
+
+	id: 'xiaomi_motion_sensor',
+
+	match: function(device) {
+		let modelId = device.attribute(1, 0, 5) && device.attribute(1, 0, 5).value;
+		if (ELIGIBLE_MODEL_ID.includes(modelId)) return 1000;
+	},
+
+	values: {
+		modelId:     { type:'string', attribute: {id: '0x0001 0x0000 0x0005' }  },
+		battery:     { type:'float',  attribute: {id: '0x0001 0x0000 0xff01', toValue: ((data, valueObj) => { return (data && data.length > 3) ? (data[3].charCodeAt(0)*256 + data[2].charCodeAt(0))/1000 : undefined; }), unit: 'V', min:0.00, max: 3.00 } },
+
+		temperature: { type:'float',  attribute: {id: '0x0001 0x0402 0x0000', toValue: ((attrval) => typeof(attrval) === 'number' ? (attrval/100) : undefined) }, unit: '%'  },
+		humidity:    { type:'float',  attribute: {id: '0x0001 0x0405 0x0000', toValue: ((attrval) => typeof(attrval) === 'number' ? (attrval/100) : undefined) }, unit: '%'  , min:-0,  max: 100  },
+	},
+
+	/*
+	events: {
+		motion:    { attribute: {id: '0x0001 0x0000 0x0005', equal: 1}, },
+	},
+	*/
+
+};
